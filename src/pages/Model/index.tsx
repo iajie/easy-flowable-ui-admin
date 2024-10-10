2903d2d811066669ee719396bcb5a0aae89465e8
阿杰 <mojie@z-srm.com>
2024-10-10 17:58:33 +0800

    feat: 组件传递用户列表

--- src/pages/Model/index.tsx
+++ src/pages/Model/index.tsx
@@ -140,7 +140,7 @@ export default () => {
                 }}
                 panel={{
                     users,
-                    groups
+                    groups,
                 }}
                 data={state.dataInfo.modelEditorXml}
                 flowKey={state.dataInfo.key}
